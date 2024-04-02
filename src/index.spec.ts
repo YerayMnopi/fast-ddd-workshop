ab48ee0cc60a322a14c06a346710695f41cdbb43
Yeray Díaz Rodríguez <yeray.diaz@mongodb.com>
2024-04-02 04:16:28 +0200

    Fix path

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import request from 'supertest';
-import app from './index';
+import app from './index.js';
 
 describe('GET /user', function () {
   it('responds with json', async () => {
